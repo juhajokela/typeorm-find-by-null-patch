bc38b5dac815c9ba8d6c9e78cf1ea6d4e68f7cb6
Juha Jokela <juha@jokela>
2023-03-28 08:00:20 +0300

    Added type definitions

--- src/patch.ts
+++ src/patch.ts
@@ -1,11 +1,23 @@
 import { strict as assert } from 'node:assert'
-import { IsNull } from "typeorm"
+import {
+  EntityTarget,
+  ObjectLiteral,
+  IsNull,
+  FindManyOptions,
+  FindOptionsWhere,
+  FindOneOptions,
+  DataSource,
+} from "typeorm"
+
+interface PatchedDataSource extends DataSource {
+  _getRepository?: DataSource['getRepository']
+}
 
 // quick & dirty SHALLOW copy, for better (preferably deep copy?) check lodash or something else
-const copy = (obj) => ({ ...obj })
+const copy = (obj: Object) => ({ ...obj })
 
-const isArray = (obj) =>  !!obj && obj.constructor === Array
-const isObject = (obj) => !!obj && obj.constructor === Object
+const isArray = (obj: any) =>  !!obj && obj.constructor === Array
+const isObject = (obj: any) => !!obj && obj.constructor === Object
 
 function mapValue(value) {
   if (value === null) {
@@ -20,62 +32,75 @@ function mapValue(value) {
   return value
 }
 
-const mapArray = array => array.map(x => mapValue(x))
-const mapEntries = ([key, value]) => [key, mapValue(value)]
-const mapObject = obj => Object.fromEntries(Object.entries(obj).map(mapEntries))
+const mapArray = (array: any[]): any[] => array.map((x) => mapValue(x))
+const mapEntries = ([key, value]: [string, any]): [string, any] => [key, mapValue(value)]
+const mapObject = (obj: Object): Object => Object.fromEntries(Object.entries(obj).map(mapEntries))
 
 // https://typeorm.io/custom-repository
 // https://github.com/typeorm/typeorm/blob/0.3.12/src/repository/Repository.ts#L523
 // https://typeorm.io/working-with-entity-manager
 // https://github.com/typeorm/typeorm/blob/0.3.12/src/entity-manager/EntityManager.ts#L1071
 
-const getRepositoryProxy = (db) =>
-  (entityClass) => db._getRepository(entityClass).extend({
-    async find(options) {
-      const opts = copy(options)
-      opts.where = mapObject(options.where)
-      return this.manager.find(entityClass, opts)
-    },
-    async findBy(where) {
-      return this.manager.findBy(entityClass, mapObject(where))
-    },
-    async findAndCount(options) {
-      const opts = copy(options)
-      opts.where = mapObject(options.where)
-      return this.manager.findAndCount(entityClass, opts)
-    },
-    async findAndCountBy(where) {
-      return this.manager.findAndCountBy(entityClass, mapObject(where))
-    },
-    /*
-    async findByIds(ids) {
-      return this.manager.findByIds(entityClass, ids.map(mapNull))
-    },
-    */
-    async findOne(options) {
-      const opts = copy(options)
-      opts.where = mapObject(options.where)
-      return this.manager.findOne(entityClass, opts)
-    },
-    async findOneBy(where) {
-      return this.manager.findOneBy(entityClass, mapObject(where))
-    },
-    /*
-    async findOneById(id) {
-      return this.manager.findOneById(entityClass, mapNull(id))
-    },
-    */
-    async findOneOrFail(options) {
-      const opts = copy(options)
-      opts.where = mapObject(options.where)
-      return this.manager.findOneOrFail(entityClass, opts)
-    },
-    async findOneByOrFail(where) {
-      return this.manager.findOneByOrFail(entityClass, mapObject(where))
-    }
-  })
+const getRepositoryProxy = (db: PatchedDataSource) =>
+  <Entity extends ObjectLiteral>(entityClass: EntityTarget<Entity>) =>
+    db._getRepository(entityClass).extend({
+      async find(options?: FindManyOptions<Entity>): Promise<Entity[]> {
+        const opts = copy(options) as FindManyOptions<Entity>
+        opts.where = mapObject(options.where) as FindOptionsWhere<Entity>
+        return this.manager.find(entityClass, opts)
+      },
+      async findBy(
+        where: FindOptionsWhere<Entity> | FindOptionsWhere<Entity>[],
+      ): Promise<Entity[]> {
+        return this.manager.findBy(entityClass, mapObject(where))
+      },
+      findAndCount(
+        options?: FindManyOptions<Entity>,
+      ): Promise<[Entity[], number]> {
+        const opts = copy(options) as FindManyOptions<Entity>
+        opts.where = mapObject(options.where) as FindOptionsWhere<Entity>
+        return this.manager.findAndCount(entityClass, opts)
+      },
+      findAndCountBy(
+        where: FindOptionsWhere<Entity> | FindOptionsWhere<Entity>[],
+    ): Promise<[Entity[], number]> {
+        return this.manager.findAndCountBy(entityClass, mapObject(where))
+      },
+      /*
+      async findByIds(ids: any[]): Promise<Entity[]> {
+        return this.manager.findByIds(entityClass, ids.map(mapNull))
+      },
+      */
+      async findOne(options: FindOneOptions<Entity>): Promise<Entity | null> {
+        const opts = copy(options) as FindManyOptions<Entity>
+        opts.where = mapObject(options.where) as FindOptionsWhere<Entity>
+        return this.manager.findOne(entityClass, opts)
+      },
+      async findOneBy(
+        where: FindOptionsWhere<Entity> | FindOptionsWhere<Entity>[],
+      ): Promise<Entity | null> {
+        return this.manager.findOneBy(entityClass, mapObject(where))
+      },
+      /*
+      async findOneById(
+          id: number | string | Date | ObjectID,
+      ): Promise<Entity | null> {
+        return this.manager.findOneById(entityClass, mapNull(id))
+      },
+      */
+      async findOneOrFail(options: FindOneOptions<Entity>): Promise<Entity> {
+        const opts = copy(options) as FindManyOptions<Entity>
+        opts.where = mapObject(options.where) as FindOptionsWhere<Entity>
+        return this.manager.findOneOrFail(entityClass, opts)
+      },
+      async findOneByOrFail(
+        where: FindOptionsWhere<Entity> | FindOptionsWhere<Entity>[],
+      ): Promise<Entity> {
+        return this.manager.findOneByOrFail(entityClass, mapObject(where))
+      }
+    })
 
-export default function patch(db) {
+export default function patch(db: PatchedDataSource) {
   assert.strictEqual(db._getRepository, undefined)
   db._getRepository = db.getRepository
   db.getRepository = getRepositoryProxy(db)
